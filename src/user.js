40c52c912801c4443b6556c3e357715f37141526
Bruno Beserra Vieira <brunobeserravieira@gmail.com>
2020-08-24 16:54:17 -0300

    fix create user

--- src/user.js
+++ src/user.js
@@ -14,21 +14,21 @@ module.exports = {
 
     try {
       const usersWithThisUsername = await getUserByUsername(value.username);
-      if (usersWithThisUsername.length > 0) {
+      if (usersWithThisUsername.rows.length > 0) {
         return Util.envelop("username already taken", 400);
       }
 
       const usersWithThisEmail = await getUserByEmail(value.email);
-      if (usersWithThisEmail.length > 0) {
+      if (usersWithThisEmail.rows.length > 0) {
         return Util.envelop("email already taken", 400);
       }
 
       value.password = bcrypt.hashSync(value.password, 5);
       let res = insertUser(value);
 
-      return Util.envelop(res, 201);
+      return Util.envelop(res, 200);
     } catch (err) {
-      return Util.envelop(err.message, 500);
+      return Util.envelop(err.stack, 500);
     }
   },
   async get(event) {
@@ -38,25 +38,23 @@ module.exports = {
 
 const getUserByUsername = async (username) => {
   return await pool.query(
-    "getByUsername",
-    "SELECT id, username, email, password, created_on, last_login FROM public.users WHERE username = $2",
+    "SELECT id, username, email, password, created_on, last_login FROM public.users WHERE username = $1",
     [username]
   );
 };
 
 const getUserByEmail = async (email) => {
   return await pool.query(
-    "getByEmail",
-    "SELECT id, username, email, password, created_on, last_login FROM public.users WHERE email = $2",
+    "SELECT id, username, email, password, created_on, last_login FROM public.users WHERE email = $1",
     [email]
   );
 };
 
 const insertUser = async (user) => {
   const text =
-    "INSERT INTO public.users(username, email, password) VALUES($1, $2, $3) RETURNING *";
+    "INSERT INTO users(username, email, password) VALUES($1, $2, $3) RETURNING *";
   const values = [user.username, user.email, user.password];
-  return await pool.query("insertUser", text, values);
+  return await pool.query(text, values).rows[0];
 };
 
 const userSchema = Joi.object({

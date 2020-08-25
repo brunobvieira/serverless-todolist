38d533346db2b39cba67388caaba07a777ed9df0
Bruno Beserra Vieira <brunobeserravieira@gmail.com>
2020-08-25 13:12:03 -0300

    todo model

---
+++ models/Todo.js
@@ -0,0 +1,28 @@
+const { DataTypes } = require("sequelize");
+const User = require("./User");
+
+module.exports = (sequelize) => {
+    return sequelize.define("todos", {
+        id: {
+            type: DataTypes.BIGINT,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        user_id: {
+            type: DataTypes.BIGINT,
+            references: {
+                model: User(sequelize),
+                key: "id"
+            }
+        },
+        description: {
+            type: DataTypes.STRING(50),
+            allowNull: false
+        },
+        done: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        }
+    });
+};

a80923a43c0afbc1e80fb4e8547f3b13d111a49b
Vinícius Regis <vini.reg@gmail.com>
2020-08-25 18:33:02 -0300

    Utilizando banco local quando offline

--- shared/database.js
+++ shared/database.js
@@ -2,18 +2,27 @@
 
 const { Sequelize } = require("sequelize");
 
-module.exports = new Sequelize(
-  process.env.DBDATABASE,
-  process.env.DBUSER,
-  process.env.DBPASSWORD,
-  {
-    host: process.env.DBHOST,
-    dialect: process.env.DBDIALECT,
-    port: process.env.DBPORT,
-    pool: {
-      min: 0,
-      max: 1,
-      idle: 10000,
-    },
-  }
-);
+if(process.env.IS_OFFLINE) {
+  module.exports = new Sequelize({
+    dialect: 'sqlite',
+    storage: 'db.sqlite'
+  });
+} else {
+  module.exports = new Sequelize(
+    process.env.DBDATABASE,
+    process.env.DBUSER,
+    process.env.DBPASSWORD,
+    {
+      host: process.env.DBHOST,
+      dialect: process.env.DBDIALECT,
+      port: process.env.DBPORT,
+      pool: {
+        min: 0,
+        max: 1,
+        idle: 10000,
+      },
+    }
+  );
+}
+
+

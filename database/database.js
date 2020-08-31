72038f25f57d654183c5c5729b0b53590165f314
Bruno Beserra VIeira <brunobeserravieira@gmail.com>
2020-08-31 16:05:47 -0300

    setup ci/cd migration

--- database/database.js
+++ database/database.js
@@ -4,6 +4,20 @@ if (process.env.IS_OFFLINE || process.env.NODE_ENV == "local") {
         dialect: "sqlite",
         storage: "database/db.sqlite"
     };
+} else if (process.env.NODE_ENV == "development") {
+    module.exports = {
+        host: process.env.DBHOST_DEV,
+        username: process.env.DBUSER_DEV,
+        password: process.env.DBPASSWORD_DEV,
+        database: process.env.DBDATABASE_DEV,
+        dialect: process.env.DBDIALECT,
+        port: process.env.DBPORT,
+        pool: {
+            min: 0,
+            max: 1,
+            idle: 10000
+        }
+    };
 } else {
     module.exports = {
         host: process.env.DBHOST,
